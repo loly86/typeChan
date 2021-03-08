9df6b8ec99e8e459c9cc411f999f2c5ba32da2b2
loly86 <loly6@naver.com>
2021-03-09 01:20:41 +0900

    goo dev1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-   return `Hello ${name}, you are ${age}, you are a ${gender}`
+import * as CryptoJs from "crypto-js"
 
+class Block {
+   public index: number
+   public hash: string
+   public previousHash: string
+   public data: string
+   public timestamp: number
+
+   static calculateBlockHash = (
+      index: number,
+      previousHash: string,
+      timestamp: number,
+      data: string
+   ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString()
+
+   constructor(
+      index: number,
+      hash: string,
+      previousHash: string,
+      data: string,
+      timestamp: number
+   ) {
+      this.index = index
+      this.hash = hash
+      this.previousHash = previousHash
+      this.data = data
+      this.timestamp = timestamp
+
+   }
 }
 
-console.log(sayHi("minsoo", 24, "male"))
+const genesisBlock: Block = new Block(0, "202020202020202", "", "Hello", 123456)
+let blockchain: Block[] = [genesisBlock]
+
+const getBlockchain =(): Block[] => blockchain
+const getLatesBlock =(): Block => blockchain[blockchain.length - 1]
+const getNewTimeStamp =(): number => Math.round(new Date().getTime() / 1000)
+
 
 export { };
