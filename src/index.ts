56f8f30a8b8b34204146f90c8bf6ad11b87d2c5d
loly86 <loly6@naver.com>
2021-03-13 20:00:29 +0900

    210313 fix

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,18 @@ class Block {
       data: string
    ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString()
 
+   static validateStructure = (aBlock: Block): boolean =>
+      typeof aBlock.index === "number" &&
+      typeof aBlock.hash === "string" &&
+      typeof aBlock.previousHash === "string" &&
+      typeof aBlock.data === "string"
+
+   // public index: number
+   // public hash: string
+   // public previousHash: string
+   // public data: string
+   // public timestamp: number
+
    constructor(
       index: number,
       hash: string,
@@ -33,9 +45,44 @@ class Block {
 const genesisBlock: Block = new Block(0, "202020202020202", "", "Hello", 123456)
 let blockchain: Block[] = [genesisBlock]
 
-const getBlockchain =(): Block[] => blockchain
-const getLatesBlock =(): Block => blockchain[blockchain.length - 1]
-const getNewTimeStamp =(): number => Math.round(new Date().getTime() / 1000)
+const getBlockchain = (): Block[] => blockchain
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data: string): Block => {
+   const previousBlock: Block = getLatestBlock()
+   const newIndex: number = previousBlock.index + 1
+   const newTimestamp: number = getNewTimeStamp()
+   const nextHash: string = Block.calculateBlockHash(
+      newIndex,
+      previousBlock.hash,
+      newTimestamp,
+      data
+   )
+   const newBlock: Block = new Block(
+      newIndex,
+      nextHash,
+      previousBlock.hash,
+      data,
+      newTimestamp,
+   )
+   return newBlock
+}
+
+const isBlockValid = (candidateBlock: Block,
+   previousBlock: Block
+): boolean => {
+   if (Block.validateStructure(candidateBlock)) {
+      return false
+   } else if (previousBlock.index + 1 !== candidateBlock.index) {
+      return false
+   } else if (previousBlock.hash !== candidateBlock.previousHash) {
+      return false
+   }
+
+}
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"))
 
 
 export { };
